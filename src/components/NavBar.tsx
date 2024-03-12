b0a9d621e10035627635eca803cc1b9106543f49
Jennifer Amachree <jennamachree@gmail.com>
2024-03-12 14:34:40 -0400

    The calendarView adjusts to screensize and drop down

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,17 @@ import './NavBar.css'
 const NavBar: React.FC = () => {
     return (
         <div className="navbar">
-            
-            <img src = '../../public/logo.png'></img>
+            <div className="dropdown">
+                <button className="dropbtn">
+                    <i className="fa fa-bars"></i> {/* Stack icon */}
+                </button>
+                <div className="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                    <a href="#">Option 3</a>
+                </div>
+            </div>
+            <img src = '../../logo.png'></img>
             {/* Add navigation elements here */}
         </div>
     );
