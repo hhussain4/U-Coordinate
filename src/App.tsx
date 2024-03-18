fccdacdacfbdc14b3939d2c503694dbf8490ad88
BlakeCaraballo <blakecaraballo1@gmail.com>
2024-03-17 21:13:44 -0400

    darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calendar from '@pages/CalendarView';
 import NotFound from '@pages/NotFound';
+import DarkModeToggle from './components/DarkMode'; // Import DarkModeToggle component
 
 function App() {
-  // make index element whatever we decide to be the home page
   return (
     <div>
       <BrowserRouter>
+        {/* Add the DarkModeToggle component outside the Routes */}
+        <DarkModeToggle />
         <Routes>
           <Route index element={<Calendar />} />
           <Route path="/calendar" element={<Calendar />} />
